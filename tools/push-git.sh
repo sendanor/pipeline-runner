f485f0778ad50b46646feaf6bd55bc21e2bce76d
Jaakko Heusala <jheusala@iki.fi>
2021-09-04 09:31:35 +0300

    Updated git update scripts

--- tools/push-git.sh
+++ tools/push-git.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Push git and submodules to upstream
-
 (
   set -e
   set -x
 
-  git push
-  (cd src/nor/pipeline/ && git push)
-  (cd src/nor/ts/ && git push)
-  (cd src/nor/matrix/ && git push)
+  git push -q
+  if test -d src/nor/matrix/   ; then ( cd src/nor/matrix/   && git push -q ); fi
+  if test -d src/nor/pipeline/ ; then ( cd src/nor/pipeline/ && git push -q ); fi
+  if test -d src/nor/ts/       ; then ( cd src/nor/ts/       && git push -q ); fi
+  if test -d src/nor/ui/       ; then ( cd src/nor/ui/       && git push -q ); fi
 
 ) && echo Success || echo FAIL
--- tools/update-git.sh
+++ tools/update-git.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Update git and submodules from upstream
-
 (
   set -e
   set -x
 
-  git pull
-  (cd src/nor/pipeline/ && git pull)
-  (cd src/nor/ts/ && git pull)
-  (cd src/nor/matrix/ && git pull)
+  git pull -q
+  if test -d src/nor/matrix/   ; then ( cd src/nor/matrix/   && git pull -q ); fi
+  if test -d src/nor/pipeline/ ; then ( cd src/nor/pipeline/ && git pull -q ); fi
+  if test -d src/nor/ts/       ; then ( cd src/nor/ts/       && git pull -q ); fi
+  if test -d src/nor/ui/       ; then ( cd src/nor/ui/       && git pull -q ); fi
 
 ) && echo Success || echo FAIL
